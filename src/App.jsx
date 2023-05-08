34476f772536497879ddeee332287caa12b65c11
rosenfence <rosenfence@gmail.com>
2023-05-08 15:10:28 +0900

    feat: Lottie Animation 추가

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Lottie from './components/Lottie';
+
+function App() {
+  return (
+    <div className='App'>
+      <Lottie listIdx='0' />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Lottie.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect, useRef } from 'react';
+import lottie from 'lottie-web';
+import logo from '../constants/logo.json';
+
+const Lottie = ({ listIdx }) => {
+  const container = useRef();
+  const list = [logo];
+
+  useEffect(() => {
+    lottie.loadAnimation({
+      container: container.current,
+      renderer: 'svg',
+      loop: false,
+      autoplay: true,
+      animationData: list[listIdx],
+    });
+    // eslint-disable-next-line
+  }, []);
+
+  return <div ref={container}></div>;
+};
+
+export default Lottie;
