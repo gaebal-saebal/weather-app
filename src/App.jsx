c77c82c3a338c222a7659403c604cc8ead2a0b2c
js-ha <jshaha0911@gmail.com>
2023-05-08 18:46:44 +0900

    feat : weather api get 요청

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 import { Button } from '@mui/material';
 import Lottie from './components/Lottie';
 import { getXY } from './functions/getXY';
+import { useState } from 'react';
 
 function App() {
+  const [weather, setWeather] = useState([]);
+
   let today = new Date();
   let year = today.getFullYear();
   let month = ('0' + (today.getMonth() + 1)).slice(-2);
   let day = ('0' + today.getDate()).slice(-2);
-  let hours = ('0' + today.getHours()).slice(-2);
+  let hours = ('0' + today.getHours()).slice(-2) - 1;
   let minutes = ('0' + today.getMinutes()).slice(-2);
 
   const data = {
-    serviceKey: process.env.REACT_APP_SERVICEKEY,
+    serviceKey:
+      'IvPYwwrcAkf4XhepyiPms9XJvyn7XFnY6FLRx57UDVoUsvT71YAo8%2BQsEmOz1XzNiICh3Kg1P%2F%2FLMDaihP6OmQ%3D%3D',
     numOfRows: '10',
     pageNo: '1',
     dataType: 'JSON',
@@ -20,6 +24,7 @@ function App() {
     nx: 43,
     ny: 136,
   };
+  const URL = `http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst?serviceKey=${data.serviceKey}&numOfRows=${data.numOfRows}&pageNo=${data.pageNo}&dataType=${data.dataType}&base_date=${data.baseDate}&base_time=${data.baseTime}&nx=${data.nx}&ny=${data.ny}`;
 
   try {
     navigator.geolocation.getCurrentPosition(function (pos) {
@@ -31,10 +36,33 @@ function App() {
   } catch (err) {
     console.log('문제발생');
   }
+
+  const handleClick = () => {
+    fetch(URL)
+      .then((res) => console.log(res.json()))
+      .then((data) => {
+        const weatherData = data.response.body.items.item;
+        setWeather(weatherData);
+        console.log(weatherData);
+      });
+
+    // const response = await fetch(URL);
+    // if (response.ok) {
+    //   const jsonValue = await response.json();
+    //   console.log(jsonValue);
+    //   return Promise.resolve(jsonValue);
+    // } else {
+    //   return Promise.reject('안됨');
+    // }
+  };
+  console.log(weather);
   return (
     <div className='App'>
       <Lottie listIdx='0' />
-      <Button variant='outlined'>현재 날씨 알아보기</Button>
+      <Button onClick={handleClick} variant='outlined'>
+        현재 날씨 알아보기
+      </Button>
+      {/* <p>{weather}</p> */}
     </div>
   );
 }
