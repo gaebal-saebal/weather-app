a9fa018bbb08619f33d5a39a721c8f58117df7fb
js-ha <jshaha0911@gmail.com>
2023-05-09 16:55:19 +0900

    feat : 강수형태 판독 함수 구현

---
+++ src/constants/rainTypeFn.js
@@ -0,0 +1,27 @@
+export const rainTypeFn = (fcstValue) => {
+  let result = '';
+  switch (fcstValue) {
+    case '0':
+      result = '없음';
+      break;
+    case '1':
+      result = '비';
+      break;
+    case '2':
+      result = '비/눈';
+      break;
+    case '3':
+      result = '눈';
+      break;
+    case '5':
+      result = '빗방울';
+      break;
+    case '6':
+      result = '빗방울눈날림';
+      break;
+    case '7':
+      result = '눈날림';
+      break;
+  }
+  return result;
+};
